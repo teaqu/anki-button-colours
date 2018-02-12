2c3860e8f02b5a4f39f1c8744c5e330ce15832d3
C9HDN <c9h0xdn@gmail.com>
2018-02-11 23:50:57 -0500

    Anki 2.1

---
+++ button_colours/__init__.py
@@ -0,0 +1,16 @@
+# Author: C9HDN
+# Get reviewer class
+from aqt.reviewer import Reviewer
+
+# Replace _answerButtonList method
+def answerButtonList(self):
+    l = ((1, "<font color='red'>" + _("Again") + "</font>"),)
+    cnt = self.mw.col.sched.answerButtons(self.card)
+    if cnt == 2:
+        return l + ((2, "<font color='green'>" + _("Good") + "</font>"),)
+    elif cnt == 3:
+        return l + ((2, "<font color='green'>" + _("Good") + "</font>"), (3, "<font color='blue'>" + _("Easy") + "</font>"))
+    else:
+        return l + ((2, _("Hard")), (3, "<font color='green'>" + _("Good") + "</font>"), (4, "<font color='blue'>" + _("Easy") + "</font>"))
+
+Reviewer._answerButtonList = answerButtonList
